3976f0ae33b93e4024c172aa93473e427f272c42
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2021-05-14 17:08:47 +0000

    Update

--- testScrap.py
+++ testScrap.py
@@ -21,4 +21,32 @@ if table is not None:
     results = table.find_all('tr')
     print('Number of results', len(results))
     
+    
+    # create and write headers to a list
+    rows = []
+    rows.append(['Rank', 'Company Name', 'Webpage', 'Description', 'Location', 'Year end', 'Annual sales rise over 3 years', 'Sales £000s', 'Staff', 'Comments'])
+    print(rows)
+    
+    # loop over results
+    for result in results:
+        # find all columns per result
+        data = result.find_all('td')
+        
+        if len(data) !=0 :
+            # write columns to variables
+            rank = data[0].getText()
+            company = data[1].getText()
+            location = data[2].getText()
+            yearend = data[3].getText()
+            salesrise = data[4].getText()
+            sales = data[5].getText()
+            staff = data[6].getText()
+            comments = data[7].getText()
+            print(company)
+        
+        # check that columns have data
+        elif len(data) == 0:
+            continue
+        
+    
 print("End ....")
